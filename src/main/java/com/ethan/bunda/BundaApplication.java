04376d7cdf7af066189c24ead10de428fdeb67e2
Seonghun <Seonghun337@github.com>
2022-05-23 22:12:07 +0900

    first commit

---
+++ src/main/java/com/ethan/bunda/BundaApplication.java
@@ -0,0 +1,13 @@
+package com.ethan.bunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BundaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ethan/bunda/BundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethan.bunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
